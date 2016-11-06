d026cea33275d1c0bd2d4c173612b7de891aa500
anastasiya0304 <89684777770@mail.ru>
2016-11-07 00:12:03 +0300

    Update shared_ptr.hpp

--- include/shared_ptr.hpp
+++ include/shared_ptr.hpp
@@ -1,1 +1,22 @@
+#include <iostream>
+#include <utility>
+#include <stdexcept>
 
+template <typename T>
+class shared_ptr {
+public:
+    shared_ptr();
+    shared_ptr(T* ptr);
+    auto operator= (shared_ptr const & other)->shared_ptr &;
+    auto operator =(shared_ptr && other) -> shared_ptr &;
+    auto swap(shared_ptr& r) noexcept -> void;
+    auto reset() -> void;
+    auto operator ->() const -> T *;
+    auto operator *() const -> T *;
+    ~shared_ptr();
+    auto count() const->size_t;
+ 
+private:
+    T* ptr_;
+    size_t *count_;
+};
