7d7b9106d109c7688821ca8012d6ff47d3a72700
anastasiya0304 <89684777770@mail.ru>
2016-11-14 18:43:15 +0400

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -1,44 +1,58 @@
-#include "shared_ptr.hpp"
+#include "sharedptr.hpp"
 #include <catch.hpp>
-#include <iostream>
 
- 
-SCENARIO("count and get for empty shared", "[count&get]"){
- shared_ptr<size_t> s;
-  REQUIRE(s.count()==0);
- REQUIRE(s.get()==nullptr);
+SCENARIO("Init", "[Init]"){
+  shared_ptr<int> a;
+  REQUIRE(a.use_count() == 0);
+  REQUIRE(a.get() == nullptr);
+  REQUIRE(!bool(a));
 }
 
-SCENARIO("get for shared initialize", "[getInShared]"){
- shared_ptr<size_t> s(new size_t[10]);
- REQUIRE(s.get()!=nullptr);
+SCENARIO("with parameteres", "[with param]"){
+  shared_ptr<int> a(new int(5));
+  REQUIRE(a.use_count() == 1);
+  REQUIRE(*a == 5);
+  REQUIRE(a.unique());
+  //REQUIRE(a);
 }
-SCENARIO("Initialize Shared", "[InShGet]") {
-	size_t * ptr = new size_t[10];
-	shared_ptr<size_t> s(ptr);
 
-	REQUIRE(ptr == s.get());
+SCENARIO("copy constructor", "[copy c]"){
+  shared_ptr<int> a(new int(5));
+  shared_ptr<int> b(a);
+  REQUIRE(*b == 5);
+  REQUIRE(b.use_count() == 2);
 }
-SCENARIO("Checking count", "[CheckCount]") {
-	size_t * ptr = new size_t[10];
-	shared_ptr<size_t> f_sp(ptr);
 
-	REQUIRE(f_sp.count() == 1);
-
-	shared_ptr<size_t> s_sp(f_sp);
-
-	REQUIRE(f_sp.count() == 2);
-	REQUIRE(s_sp.count() == 2);
-
-	shared_ptr<size_t> t_sp(s_sp);
-
-	REQUIRE(f_sp.count() == 3);
-	REQUIRE(s_sp.count() == 3);
-	REQUIRE(t_sp.count() == 3);
+SCENARIO("copy operator", "[copy o]"){
+  shared_ptr<int> a(new int(5));
+  shared_ptr<int> b;
+  b = a;
+  REQUIRE(*b == 5);
+  REQUIRE(b.use_count() == 2);
 }
-SCENARIO("My","[MY]"){
 
-shared_ptr<size_t> s;
-shared_ptr<size_t> s2(s);
-REQUIRE(s2.count()==0);
+SCENARIO("move constructor", "[move c]"){
+  shared_ptr<int> a(new int(5));
+  shared_ptr<int> b(std::move(a));
+  REQUIRE(*b == 5);
+  REQUIRE(a.get() == nullptr);
+  REQUIRE(b.use_count() == 1);
+  REQUIRE(a.use_count() == 0);  
+}
+
+SCENARIO("move operator", "[move o]"){
+  shared_ptr<int> a(new int(5));
+  shared_ptr<int> b;
+  b = std::move(a);
+  REQUIRE(*b == 5);
+  REQUIRE(a.get() == nullptr);
+  REQUIRE(b.use_count() == 1);
+  REQUIRE(a.use_count() == 0);
+}
+
+SCENARIO("reset", "[reset]"){
+  shared_ptr<int> a(new int(5));
+  a.reset();
+  REQUIRE(a.use_count() == 0);
+  REQUIRE(a.get() == nullptr);
 }
